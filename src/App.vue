9cbb0d4d3a4704faa7097a7afe5f3a05f8a9b205
Tyra Parring <parringt@gmail.com>
2024-07-02 15:11:26 +0200

    finished about page

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div id="app">
     <NavBarComp />
     <router-view /> 
+    <!-- <SpinnerComp/> -->
     <FooterComp />
   </div>
 </template>
@@ -9,12 +10,14 @@
 <script>
 import NavBarComp from './components/NavBarComp.vue';
 import FooterComp from './components/FooterComp.vue';
+// import SpinnerComp from './components/SpinnerComp.vue'
 
 export default {
   name: 'App',
   components: {
     NavBarComp,
-    FooterComp
+    FooterComp,
+    // SpinnerComp
   }
 };
 </script>
