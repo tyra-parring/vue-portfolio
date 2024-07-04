231495c43e89e1a455d93d7d3e477fb7d8988a27
Tyra Parring <parringt@gmail.com>
2024-07-04 11:00:52 +0200

    created resume page

--- src/views/ResumeView.vue
+++ src/views/ResumeView.vue
@@ -1,13 +1,53 @@
-<template lang="">
-    <div>
-        
+<template>
+    <div class="background-img">
+      <div class="container">
+        <section class="edu">
+          <h1 class="resu">RESUME</h1>
+          <h2>Education</h2>
+          <div class="education-content">
+            <div class="edu-item">
+              <h3>2019 - 2022</h3>
+              <p>I completed my schooling career at Bonteheuwel High School.</p>
+            </div>
+            <div class="edu-item">
+              <h3>2021</h3>
+              <p>Completed a Reading and Writing Tutoring Workshop under the Amy Biehl Foundation.</p>
+            </div>
+            <div class="edu-item">
+              <h3>2024 - 2025</h3>
+              <p>I joined a Coding Bootcamp program that provided the fundamentals of Web Development, including learning coding languages such as HTML, CSS, JavaScript, and more. The bootcamp also taught interpersonal and self-development skills to help us grow both professionally and personally.</p>
+            </div>
+          </div>
+        </section>
+  
+        <section class="work">
+          <h2>Work Experience</h2>
+          <div class="work-content">
+            <div class="work-item">
+              <h3>2022 - FUTUR-ED E-LEARNING CENTRES</h3>
+              <p>Completed a job shadowing programme at a contact centre, promoting products to customers.</p>
+            </div>
+          </div>
+        </section>
+      </div>
     </div>
-</template>
+  </template>
+  
 <script>
+// import SpinnerComp from '../components/SpinnerComp.vue'
+//   import AOS from 'aos';
+  import 'aos/dist/aos.css';
+
 export default {
     
 }
 </script>
-<style lang="">
-    
+<style>
+  .background-img {
+    background-image: url('https://tyra-parring.github.io/host-/image/monstera-leaf-coffee-laptop-pencil-diary-marble-textured-backdrop.jpg');
+    background-size: cover;
+    background-repeat: no-repeat;
+    height: 100vh;
+    width: auto;
+  } 
 </style>
